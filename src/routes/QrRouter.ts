c93736ec347e017580beccd92d8ce849e1176913
ruslan <rusik85@gmail.com>
2019-09-24 22:25:59 +0300

    add qr doc to swagger

--- src/routes/QrRouter.ts
+++ src/routes/QrRouter.ts
@@ -6,8 +6,56 @@ import { QrComponent } from '../components';
  */
 const router: Router = Router();
 
+/**
+ * GET method route
+ * @example http://localhost:PORT/qr
+ *
+ * @swagger
+ * /qr:
+ *   get:
+ *     description: Get qr
+ *     tags: ["qr"]
+ *     responses:
+ *       200:
+ *         description: sample
+ *         content:
+ *           application/json:
+ *             schema:
+ *               oneOf:
+ *                - $ref: '#/components/schemas/Qr'
+ *       default:
+ *          description: unexpected error
+ *          content:
+ *            application/json:
+ *              schema:
+ *                $ref: '#/components/schemas/Error'
+ */
 router.get('/', QrComponent.getQr);
 
+/**
+ * GET method route
+ * @example http://localhost:PORT/qr/:id
+ *
+ * @swagger
+ * /qr:
+ *   get:
+ *     description: Get qr
+ *     tags: ["qr"]
+ *     responses:
+ *       200:
+ *         description: sample
+ *         content:
+ *           application/json:
+ *             schema:
+ *               oneOf:
+ *                - $ref: '#/components/schemas/Qr'
+ *       default:
+ *          description: unexpected error
+ *          content:
+ *            application/json:
+ *              schema:
+ *                $ref: '#/components/schemas/Error'
+ */
 router.get('/:id', QrComponent.findOne);
 
 /**
